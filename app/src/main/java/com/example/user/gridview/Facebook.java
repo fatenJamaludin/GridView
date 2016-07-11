ce934e48433f151a2ae17e8b13bec0a6dbfa708d
Nur Fatin Jamaludin <aten93.hzyra@gmail.com>
2016-07-11 16:20:06 +0800

    no message

--- app/src/main/java/com/example/user/gridview/Facebook.java
+++ app/src/main/java/com/example/user/gridview/Facebook.java
@@ -1,10 +1,17 @@
 package com.example.user.gridview;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -12,28 +19,63 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
-public class Facebook extends AppCompatActivity {
+public class Facebook extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
     private TextView info;
     private LoginButton loginButton;
+    Button sign_out_button;
     private CallbackManager callbackManager;
     ProfilePictureView friendProfilePicture;
+    private NetworkImageView profileImage;
+    private SignInButton signInButton;
+    private GoogleSignInOptions gso;
+    private GoogleApiClient mGoogleApiClient;
+
+    private static final String TAG = "LoginActivity";
+    private int RC_SIGN_IN = 100;
+    private TextView textViewName;
+    private TextView textViewEmail;
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-
         setContentView(R.layout.activity_facebook);
-        info = (TextView)findViewById(R.id.info);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-        friendProfilePicture = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
 
+        info = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        friendProfilePicture = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
+        profileImage = (NetworkImageView) findViewById(R.id.profileImage);
+        textViewName = (TextView) findViewById(R.id.textViewName);
+        textViewEmail = (TextView) findViewById(R.id.textViewEmail);
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        //signInButton.setScopes(gso.getScopeArray());
+        sign_out_button = (Button) findViewById(R.id.sign_out_button);
+
+        sign_out_button.setOnClickListener(this);
+
+        friendProfilePicture.setVisibility(View.GONE);
+        //profileImage.setVisibility(View.GONE);
+
+        //facebook
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                //info.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" + "Auth Token: " + loginResult.getAccessToken().getToken());
+                friendProfilePicture.setVisibility(View.VISIBLE);
+                signInButton.setVisibility(View.GONE);
+                sign_out_button.setVisibility(View.GONE);
                 friendProfilePicture.setProfileId(loginResult.getAccessToken().getUserId());
             }
 
@@ -47,10 +89,118 @@ public class Facebook extends AppCompatActivity {
                 info.setText("Error occured while Login");
             }
         });
+
+        //Google
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    private void signIn() {
+        //Creating an intent
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+
+        //Starting intent for result
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        //updateUI(false);
+                    }
+                });
+    }
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.login_button).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+            findViewById(R.id.login_button).setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //Calling a new function to handle signin
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        //If the login succeed
+        if (result.isSuccess()) {
+            //Getting google account
+            //updateUI(true);
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            //Displaying name and email
+            //textViewName.setText(acct.getDisplayName());
+            //textViewEmail.setText(acct.getEmail());
+
+            //Initializing image loader
+            imageLoader = CustomVolleyRequest.getInstance(this.getApplicationContext())
+                    .getImageLoader();
+
+            imageLoader.get(acct.getPhotoUrl().toString(),
+                    ImageLoader.getImageListener(profileImage,
+                            R.drawable.profile,
+                            R.drawable.profile));
+
+            //Loading image
+            profileImage.setImageUrl(acct.getPhotoUrl().toString(), imageLoader);
+
+        } else {
+            //If login fails
+            //updateUI(false);
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == signInButton) {
+            //Calling signin
+            signIn();
+        } else if (v == sign_out_button) {
+            signOut();
+        }
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
